39ab67b85889f88fbfaf2aa2c89348662bd941f9
Natan Maia <74799857+NatePlays95@users.noreply.github.com>
2022-10-10 22:47:04 -0300

    Update SRPG_GALV_EventSpawnerHelper.js

--- SRPG_GALV_EventSpawnerHelper.js
+++ SRPG_GALV_EventSpawnerHelper.js
@@ -2,9 +2,8 @@
 _existActorVarID = PluginManager.parameters('SRPG_core')['existActorVarID'];
 _existEnemyVarID = PluginManager.parameters('SRPG_core')['existEnemyVarID'];
 
-// CHANGES TO setSrpgActors AND setSrpgEnemys
-// now they ignore existing events.
-
+// CHANGES TO setSrpgEnemys, now they ignore existing events.
+// No actors just yet, Fixed Actors work in funny ways.
 // UNSPAWN is inactive cus there are faster ways to remove enemies (say, kill())
 
 Game_System.prototype.setSrpgEnemys = function() {
@@ -71,81 +70,3 @@ Game_Map.prototype.spawnEnemyGALV = function(id, x, y, save) { //again, id is fr
     $gameSystem.setAllEventType();
     $gameSystem.setSrpgEnemys();
 };
-
-// TURNS OUT ACTORS WORK FINE, ACTUALLY?
-
-
-
-// Game_System.prototype.setSrpgActors = function() {
-//     var existingActors = 0;
-    
-//     $gameMap.events().forEach(function(event) {
-//         // if event is not actor or is knocked out, go to next event
-//         if (event.isType() != 'actor' || event.isErased()) return;
-        
-//         existingActors+=1;
-        
-//         // if event is already set up, go to next event
-//         if ( $gameSystem.EventToUnit(event._eventId) != null) return;
-
-//         // else, setup new event
-//         $gameSystem.setNewSrpgActor(event);
-
-//     });
-    
-//     $gameVariables.setValue(_existActorVarID, existingActors);
-// };
-
-// Game_System.prototype.setNewSrpgActor = function(event) {
-//     if (_maxActorVarID > 0 && $gameVariables.value(_maxActorVarID) > 0 && actorNum >= $gameVariables.value(_maxActorVarID)) {
-//         event.erase();
-//         return;
-//     }
-
-
-// };
-
-// Game_System.prototype.setNewSrpgActor = function(event){
-//     //copied from SRPG_core
-//     if (_maxActorVarID > 0 && $gameVariables.value(_maxActorVarID) > 0 && 
-//         actorNum >= $gameVariables.value(_maxActorVarID)) {
-//         event.erase();
-//         return;
-//     }
-//     var actorId = event.event().meta.id ? Number(event.event().meta.id) : 0;
-//     if (actorId > 0) {
-//         var actor_unit = $gameActors.actor(actorId);
-//     } else {
-//         for (var j=0; j < array.length; j++) {
-//             var actor_unit = array[i];
-//             if (!actor_unit) {
-//                 $gameSystem.setEventToUnit(event.eventId(), 'null', null);
-//                 break;
-//             }
-//             i += 1;
-//             if (fix_actors.indexOf(actor_unit.actorId()) < 0) {
-//                 break;
-//             }
-//         };
-//     }
-//     if (actor_unit) {
-//         $gameSystem.pushSrpgAllActors(event.eventId());
-//         if (event.event().meta.mode) {
-//             actor_unit.setBattleMode(event.event().meta.mode);
-//             if (event.event().meta.targetId) {
-//                 actor_unit.setTargetId(Number(event.event().meta.targetId));
-//             }
-//         }
-//         actor_unit.setSearchItem(event.event().meta.searchItem);
-//         actor_unit.initTp(); //TPを初期化
-//         var bitmap = ImageManager.loadFace(actor_unit.faceName()); //顔グラフィックをプリロードする
-//         var oldValue = $gameVariables.value(_existActorVarID);
-//         $gameVariables.setValue(_existActorVarID, oldValue + 1);
-//         actorNum += 1;
-//         $gameSystem.setEventToUnit(event.eventId(), 'actor', actor_unit.actorId());
-//     }
-// };
-
-
-
-
